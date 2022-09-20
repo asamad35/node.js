8d89dfe4a9022cb07ef8072f360e57580f36aa8b
Your Name <you@example.com>
2022-09-20 19:20:51 +0530

    added jwt

--- authSystem/app.js
+++ authSystem/app.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
+require("./config/database").connect();
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const User = require("./model/user");
 const express = require("express");
+const user = require("./model/user");
 const app = express();
 app.use(express.json());
 
@@ -9,16 +14,40 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  const { firstname, lastname, email, password } = req.body;
+  try {
+    const { firstname, lastname, email, password } = req.body;
 
-  if (!(email && firstname && lastname && password)) {
-    res.status(400).send("all fields are required");
+    if (!(email && firstname && lastname && password)) {
+      res.status(400).send("all fields are required");
+    }
 
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       res.send(404).send("user already exists");
     }
+
+    const myEncPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      firstname,
+      lastname,
+      email: email.toLowerCase(),
+      password: myEncPassword,
+    });
+
+    //  token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    user.token = token;
+  } catch (error) {
+    console.log(error, "error in try and catch block");
   }
 });
 
