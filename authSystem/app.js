71f1b0a1a8babd9c959e5e6945a2c646c018e34e
Your Name <you@example.com>
2022-09-21 19:20:31 +0530

    login and register routes

--- authSystem/app.js
+++ authSystem/app.js
@@ -5,7 +5,6 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("./model/user");
 const express = require("express");
-const user = require("./model/user");
 const app = express();
 app.use(express.json());
 
@@ -24,7 +23,7 @@ app.post("/register", async (req, res) => {
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      res.send(404).send("user already exists");
+      res.status(404).send("user already exists");
     }
 
     const myEncPassword = await bcrypt.hash(password, 10);
@@ -46,8 +45,36 @@ app.post("/register", async (req, res) => {
     );
 
     user.token = token;
+    user.password = undefined;
+    res.status(201).json(user);
   } catch (error) {
     console.log(error, "error in try and catch block");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      user.token = token;
+      user.password = undefined;
+
+      res.status(201).json(user);
+    }
+    res.status(400).send("Password or Email is incorrect");
+  } catch (error) {
+    console.log(error);
   }
 });
 
