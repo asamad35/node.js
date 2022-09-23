1be609f37b6f8f71d505e90a0f31d719aee066c1
Your Name <you@example.com>
2022-09-23 19:22:44 +0530

    understood forms

--- formsAndImages/index.js
+++ formsAndImages/index.js
@@ -1,14 +1,54 @@
 const express = require("express");
 
 const app = express();
+const fileupload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: "dhfc9g4l0",
+  api_key: "634685325184552",
+  api_secret: "4BJNKpMabM6dWxwMQiPtaLeABtI",
+});
+
+app.set("view engine", "ejs");
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 app.get("/myget", (req, res) => {
-  console.log(req.body);
+  console.log(req.query);
 
-  res.send(req.body);
+  res.send(req.query);
 });
 
-app.listen(4001, () => console.log("Listening on port 4000..."));
+app.post("/mypost", async (req, res) => {
+  const file = req.files.samplefile;
+  const result = await cloudinary.uploader.upload(file.tempFilePath, {
+    folder: "users",
+  });
+
+  console.log(req.body);
+
+  const details = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    result,
+  };
+
+  res.send(details);
+});
+
+app.get("/mygetform", (req, res) => {
+  res.render("getForm");
+});
+app.get("/mypostform", (req, res) => {
+  res.render("postForm");
+});
+
+app.listen(4001, () => console.log("Listening on port 4001..."));
